a7cdefa1ead4eeff2a55302e0f7742517f402ff9
Alexey Kabaev <kabaev.alexey@mail.ru>
2024-06-08 17:06:03 +0300

    fixed jsonsaver.py

--- src/jsonsaver.py
+++ src/jsonsaver.py
@@ -25,14 +25,13 @@ class JsonFile(JsonABC):
 
     def write_vacancies(self, vacancy):
         """Запись Python объекта в файл"""
-        with open('data/data_file.json', 'w', encoding='utf-8') as file1:
+        with open(f'data/{self.filename}', 'w', encoding='utf-8') as file1:
             json.dump(vacancy, file1, ensure_ascii=False, indent=3, cls=Encoder)
 
     def get_info_vacancies(self):
         """Получение данных из файла"""
-        with open('data/data_file.json', 'r', encoding='utf-8') as file2:
-            data = json.load(file2)
-        return data
+        with open(f'data/{self.filename}', 'r', encoding='utf-8') as file2:
+            return json.load(file2)
 
     def del_vacancies(self, vacancy):
         """Удаление данных из файла"""
